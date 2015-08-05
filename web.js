c2ed2579eefac718b89fc6dc9e4e543ff1e5bfaf
dorisofsky <dorisofsky@gmail.com>
2015-08-05 10:06:45 +0800

    想要把地圖拉到中間?

--- web.js
+++ web.js
@@ -4,8 +4,8 @@ $(document).ready(function() {
 
 function drawTaiwan(){
 
-  var width = 900,
-  height = 700;
+  var width = 1000,
+  height = 1000;
 
   // Setting color domains(intervals of values) for our map
 
